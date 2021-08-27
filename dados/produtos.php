d174f8f67520ee3e48e3fbfd1f7a92b8164c694b
Maicon Kistemmacher <maicon.kistemmacher@gmail.com>
2021-08-27 19:10:51 -0300

    produtos

---
+++ dados/produtos.php
@@ -0,0 +1,63 @@
+<?php
+$produtos = array(
+    'goiabada' => array(
+        'nome' => 'DOCE DE LARANJA EM CALDA',
+        'descricao' => '
+            <p>Este é um doce em calda saboroso, pois contém o sabor cítrico da laranja que com a calda doce fica um show!
+
+            </p>',
+        'preco' => 22.7,
+        'peso' => '630g',
+        'imagem' => 'DOCEDELARANJEMCALDA.jpg'
+    ),
+    'doce-de-abobora' => array(
+        'nome' => 'DOCE DE LEITE COM BRIGADEIRO',
+        'descricao' => '
+            <p>Para quem ama brigadeiro, este doce é uma ótima opção para quem ama um doce após qualquer refeição ou em qualquer hora do dia!
+
+            </p>',
+        'preco' => 19.0,
+        'peso' => '650g',
+        'imagem' => 'DOCEDELEITECOMBRIGADEIRO.jpg'
+    ),
+    'pe-de-moleque' => array(
+        'nome' => 'DOCE DE LEITE COM COCO',
+        'descricao' => '
+            <p>Um delicioso doce acompanhado acompanhado com leite e coco.
+
+            </p>',
+        'preco' => 19.0,
+        'peso' => '650g',
+        'imagem' => 'DOCEDELEITECOMCOCO.jpg'
+    ),
+    'doce-de-figo' => array(
+        'nome' => 'DOCE DE LEITE EM VIDRO',
+        'descricao' => '
+            <p>Um delicioso doce com leite para te acompanhar após suas refeições ou em qualquer hora do dia!
+
+            .</p>',
+        'preco' => 20.85,
+        'peso' => '650g',
+        'imagem' => 'DOCEDELEITEEMVIDRO.jpg'
+    ),
+    'doce-de-leite' => array(
+        'nome' => 'DOCE DE FIGO EM CALDA',
+        'descricao' => '
+            <p>Esse doce de figo em calda é feito com frutas selecionadas e a qualidade da Reserva de Minas, não pode faltar em sua casa!
+
+            .</p>',
+        'preco' => 22.7,
+        'peso' => '640g',
+        'imagem' => 'DOCEDEFIGOEMCALDA.jpg'
+    ),
+    'geleia-de-jaboticaba' => array(
+        'nome' => 'DOCE DE ABÓBORA COM COCO',
+        'descricao' => '
+            <p>Existem muitas formas de preparar doce de abóbora e esse em compota com coco possui um grande sucesso, pois é possível sentir o coco, a sua textura e a abóbora macia na boca.
+
+            </p>',
+        'preco' => 21.9,
+        'peso' => '600g',
+        'imagem' => 'DOCEDEABÓBORACOMCOCO.jpg'
+    )
+);
---
+++ functions/exibe_produto.php
@@ -0,0 +1,20 @@
+<?php 
+    function exibe_produto($produto){
+?>
+    <div class="prod">
+        <div class="row flex">
+            <img src="<?php echo "images/".$produto['imagem']?>" alt="<?php echo $produto['nome']?>" class="prod-img">
+            <div class="prod-info">
+                <h1><?php echo $produto['nome']?></h1>
+                <h2>R$ <?php echo number_format($produto['preco'], 2, ',', '.')?> <span>/<?php echo $produto['peso']?></span></h2>
+            </div>
+        </div>
+        <div class="row prod-desc">
+            <?php echo $produto['descricao']?>
+        </div>
+        <div class="row" style="margin-bottom: 60px">
+            <a href="?page=contato" class="button">Eu quero!</a>
+        </div>
+    </div>
+<?php
+    }
